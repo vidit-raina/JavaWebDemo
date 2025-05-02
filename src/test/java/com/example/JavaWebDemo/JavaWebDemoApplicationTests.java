cb1f02b0820b1722b35423e145107d9edf46ab54
vidit-raina <150840699+vidit-raina@users.noreply.github.com>
2025-05-02 13:22:25 +0530

    Add files via upload

---
+++ src/test/java/com/example/JavaWebDemo/JavaWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaWebDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
